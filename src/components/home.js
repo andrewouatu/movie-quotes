863ae3435afb95d28a3da73ca9e5517c7f040f3e
Andrew Ouatu <aouatu@outlook.com>
2018-11-28 15:38:33 -0800

    Completed project setup

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores aspernatur deserunt
+            explicabo id illum labore maiores nesciunt quaerat quos, sit, tenetur? Fuga id impedit laudantium natus
+            omnis, quam quasi!</p>
+    </div>
+);
---
+++ src/components/secret_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Secret Operatives</h1>
+        <ol>
+            <li>Bear</li>
+            <li>Bo</li>
+            <li>Seth</li>
+            <li>Josh</li>
+        </ol>
+    </div>
+);
